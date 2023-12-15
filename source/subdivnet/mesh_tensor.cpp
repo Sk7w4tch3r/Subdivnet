2ad0f5c2a4e881d31c66b88cadb58f1460e49cfd
Hesam Damghanian <hdamghanian77@gmail.com>
2023-12-14 21:34:24 -0700

    feat: implement mesh_tensor methods

--- source/subdivnet/mesh_tensor.cpp
+++ source/subdivnet/mesh_tensor.cpp
@@ -1,33 +1,120 @@
 #include "mesh_tensor.h"
 
 
-MeshTensor::MeshTensor(
-                torch::Tensor faces, 
-                torch::Tensor feats,
-                std::map<std::string, std::vector<float>> cache
-                ){
-    /*
-    Parameters
-    ------------
-    faces: (N, F, 3) int32
-        Array of triangular faces.
-    feats: (N, C, F) float32
-        Array of face features.
-    Fs: (N,) int32, optional
-        Array of number of faces in each mesh. 
-        If not specified, Fs is set to n.
-    cache: dict, optional
-        things calculated from faces to avoid repeated calculation for the 
-        same mesh.
-    */
+IndexList makeIndex(
+    givde::multiatlas::DGGS const &madggs,
+    givde::Resolution const& resolution) {
+
+    IndexList workingSet = givde::multiatlas::getBaseCells(madggs);
+
+    IndexList currentChildren;
+    IndexList nextChildren;
+
+    if (resolution == givde::Resolution(0)){
+        // if resolution is 0, then we want to color the base cells
+        return workingSet;
+    } else {
+        // Level 1
+        for (auto const &cell : workingSet) {
+            for (auto const &child : givde::multiatlas::children(madggs, cell)) {
+                currentChildren.push_back(child);
+            }
+        }
+
+        // Level 2+
+        for (std::size_t i = 0; i < resolution - 1; ++i) {
+            for (auto const &child : currentChildren) {
+                for (auto const &grandChildren : givde::multiatlas::children(madggs, child)) {
+                    nextChildren.push_back(grandChildren);
+                }
+            }
+            nextChildren.swap(currentChildren);
+            nextChildren.clear();
+        }
+    }
+    return currentChildren;
+}
+
+
+
+auto idxsToMesh(
+    givde::multiatlas::DGGS const &dggs,
+    IndexList const &idxs
+    // std::vector<givde::Vec3<givde::f64>> const &colors
+) -> std::tuple<
+    givde::geometry::IGLGeometry, 
+    std::tuple<
+        std::unordered_map<int, givde::multiatlas::Index>, 
+        std::unordered_map<givde::multiatlas::Index, int>
+        >
+    > {
+    givde::i64 numFaces = idxs.size();
+
+    givde::geometry::IGLGeometry mesh;
+    auto vertSize = numFaces * 3;
+    mesh.V        = givde::MatX<givde::f64>::Zero(vertSize, 3);
+    mesh.N        = givde::MatX<givde::f64>::Zero(vertSize, 3);
+    mesh.F        = givde::MatX<givde::i64>::Zero(numFaces, 3);
+    mesh.C        = givde::MatX<givde::f64>::Zero(vertSize, 3);
+    mesh.U        = givde::MatX<givde::f64>::Zero(vertSize, 3);
+    mesh.D        = givde::MatX<givde::f64>::Zero(vertSize, 3);
+
+    // for (Eigen::Index i = 0; i < mesh.C.rows(); ++i) {
+    //     auto color = colors[i];
+    //     mesh.C.row(i) = color.transpose();
+    // }
+
+    std::unordered_map<int, givde::multiatlas::Index> faceIdxMap;
+    std::unordered_map<givde::multiatlas::Index, int> idxFaceMap;
+
+    int idx = 0;
+    for (auto const &child : idxs) {
+        auto verts       = givde::multiatlas::cellToVertsCurved(dggs, child);
+        givde::i64 v1Idx = (idx * 3) + 0;
+        givde::i64 v2Idx = (idx * 3) + 1;
+        givde::i64 v3Idx = (idx * 3) + 2;
+
+        mesh.V.row(v1Idx) = verts.at(0).raw();
+        mesh.V.row(v2Idx) = verts.at(1).raw();
+        mesh.V.row(v3Idx) = verts.at(2).raw();
+
+        mesh.D.row(v1Idx) = givde::Vec3<givde::f64>(1.0, 0.0, 0.0);
+        mesh.D.row(v2Idx) = givde::Vec3<givde::f64>(0.0, 1.0, 0.0);
+        mesh.D.row(v3Idx) = givde::Vec3<givde::f64>(0.0, 0.0, 1.0);
+
+        mesh.F.row(idx) = givde::Vec3<givde::i64>(v1Idx, v2Idx, v3Idx);
+        faceIdxMap[idx] = child;
+        idxFaceMap[child] = idx;
+        ++idx;
+    }
+    return std::make_tuple(mesh, std::make_tuple(faceIdxMap, idxFaceMap));
+}
+
+
+MeshTensor::MeshTensor(   
+    torch::Tensor faces, 
+    torch::Tensor feats,
+    std::vector<int> Fs,
+    std::unordered_map<std::string, torch::Tensor> cache){ 
+    
+    auto dggsIdxs = makeIndex(this->madggs, this->resolution);
+    auto meshIdxs = idxsToMesh(this->madggs, dggsIdxs);
+    this->dggsMesh = std::get<0>(meshIdxs);
+    auto maps = std::get<1>(meshIdxs);
+    this->faceIdxMap = std::get<0>(maps);
+    this->idxFaceMap = std::get<1>(maps);
     
     this->faces = faces;
     this->feats = feats;
     this->Fs = Fs;
     this->cache = cache;
+    this->N = this->faces.size(0);
+    this->F = this->faces.size(1);
+    this->C = this->feats.size(1);
 }
 
-MeshTensor MeshTensor::updated(torch::Tensor new_feats){
+
+MeshTensor MeshTensor::updated(torch::Tensor & new_feats){
     /*
     Return a new MeshTensor with its feats updated. 
         
@@ -35,8 +122,9 @@ MeshTensor MeshTensor::updated(torch::Tensor new_feats){
     */
     assert(new_feats.size(0) == this->feats.size(0));
     assert(new_feats.size(1) == this->feats.size(1));
-    return MeshTensor(this->faces, new_feats, this->cache);
+    return MeshTensor(this->faces, new_feats, Fs, cache);
 }
+
 
 MeshTensor MeshTensor::inverseLoopPool(std::string op, std::optional<torch::Tensor> pooled_feats){
     /*
@@ -53,70 +141,185 @@ MeshTensor MeshTensor::inverseLoopPool(std::string op, std::optional<torch::Tens
     ------------
     MeshTensor after 4-to-1 face merge.
     */
-    assert(op == "max" || op == "mean");
-}
-MeshTensor MeshTensor::loopSubdivision(){}
-MeshTensor MeshTensor::loopUnPool(){}
-std::vector<int> computeFaceAdjacencyFaces(){}
-std::vector<int> computeFaceAdjacencyReordered(){}
-MeshTensor MeshTensor::dilatedFaceAdjacencies(){} // should return jit code result
-MeshTensor MeshTensor::convolutionKernelPattern(){} // should return jit code result
-std::vector<float> aggregateVertexFeatures(){}
+    std::vector<int> pooled_Fs = this->Fs | ranges::view::transform([](int F){return F/4;}) | ranges::to<std::vector<int>>();
+    torch::Tensor pooled_faces = torch::zeros({this->faces.size(0), pooled_Fs[0], 3});
+    
+    // reindex
 
+    if (pooled_feats.has_value()){
+        assert(pooled_feats.value().size(0) == this->N);
+        assert(pooled_feats.value().size(2) == this->F/4);
+    } else {
+        // reindex
+        if (op == "max"){
+            pooled_feats = torch::zeros({this->N, this->C, this->F/4});
+        } else if (op == "mean"){
+            pooled_feats = torch::zeros({this->N, this->C, this->F/4});
+        } else {
+            throw std::invalid_argument("op must be 'max' or 'mean'");
+        }
+    }
+    return MeshTensor(pooled_faces, pooled_feats.value(), pooled_Fs, this->cache);
+}
+
+
+// torch::Tensor MeshTensor::loopSubdivision(){
+//     torch::Tensor subdiv_faces = torch::zeros({this->N, this->F*4, 3});
+//     for (int i = 0; i < this->N; i++){ // for each mesh
+//         int V = this->faces[i].max().item<int>() + 1;
+//         torch::Tensor face = this->faces[i];
+//         int F = this->Fs[i];
+
+//         torch::Tensor E = torch::cat(
+//             {
+//                 this->faces[]})
+//     }
+//     return subdiv_faces;
+// }
+
+
+// MeshTensor MeshTensor::loopUnPool(std::string mode, torch::Tensor ref_faces = torch::Tensor(), std::map<std::string, std::vector<float>> ref_cache = std::map<std::string, std::vector<float>>()){
+//     std::vector<int> unpooled_Fs = this->Fs | ranges::view::transform([](int F){return F*4;}) | ranges::to<std::vector<int>>();
+//     torch::Tensor unpooled_faces = torch::zeros({this->faces.size(0), unpooled_Fs[0], 3});
+//     torch::Tensor unpooled_feats = torch::zeros({this->feats.size(0), this->feats.size(1), unpooled_Fs[0]});
+
+//     if (ref_faces.size(0) > 0){
+//         assert(ref_faces.size(0) == this->faces.size(0));
+//         assert(ref_faces.size(1) == this->faces.size(1));
+//         assert(ref_faces.size(2) == 3);
+//         unpooled_faces = ref_faces;
+//     } else {
+//         unpooled_faces = this->loopSubdivision();
+//     }
+
+
+//     if (mode == "nearest"){
+//         unpooled_feats = torch::cat({this->feats, this->feats, this->feats, this->feats}, 2);
+//     } else if (mode == "bilinear"){
+
+//     } else {
+//         throw std::invalid_argument("mode must be 'nearest' or 'bilinear'");
+//     }
+
+
+//     return MeshTensor(ref_faces, this->feats, unpooled_Fs, ref_cache);
+// }
+
+// torch::Tensor MeshTensor::computeFaceAdjacencyFaces(){
+//     torch::Tensor FAF = torch::zeros({this->faces.size(0), this->faces.sisubtractedFeatsze(1), 3});
+//     for (int i = 0; i < this->N; i++){
+//         int F = this->Fs[i];
+//         torch::Tensor E = torch::cat({this->faces[i], torch::roll(this->faces[i], -1, 1)}, 1);
+
+//         // E_hash = E.min(1) * E.max() + E.max(1)
+//         torch::Tensor E_min = torch::stack({std::get<0>(E.min(1)), std::get<1>(E.min(1))}, 1);
+//         torch::Tensor E_hash = E_min * E.max() + std::get<0>(E.max(1));
+        
+//         // S is index of sorted E_hash.
+//         // Based on the construction rule of E,
+//         //   1. S % F is the face id
+//         //   2. S // F is the order of edge in F
+//         torch::Tensor S = torch::argsort(E_hash);
+
+//         // S[:, 0] is the face id of the first edge in E
+//         // S[:, 1] is the face id of the second edge in E
+//         S = S.reshape({F, 3, 2});
+
+//         FAF[i, S[:, 0] % F, S[:, 0] / F] = S[:, 1] % F;
+//         FAF[i, S[:, 1] % F, S[:, 1] / F] = S[:, 0] % F;
+//     }
+//     return FAF;
+// }
+
+
+torch::Tensor MeshTensor::computeFaceAdjacencyFaces(){
+    // compute face adjacency faces
+    // for each face in each mesh, find the adjacent faces (3 faces)
+
+    torch::Tensor FAF = torch::zeros({this->faces.size(0), this->faces.size(1), 3});
+
+    for (int i = 0; i < this->N; i++){ // for each mesh
+        int F = this->Fs[i];
+        for (int j = 0; j < F; j++){ // for each face
+            if (this->faceIdxMap.find(j) != this->faceIdxMap.end()){
+                // if the face is in the faceIdxMap
+                auto idx = this->faceIdxMap[j];
+                auto neighbors = givde::multiatlas::neighbors(this->madggs, idx, givde::NeighborTypes::EDGE);
+                
+            }
+        }
+    }
+}
+
+std::tuple<torch::Tensor, torch::Tensor> MeshTensor::computeFaceAdjacencyReordered(){
+    throw std::invalid_argument("not implemented");
+    // TODO implement shift and roll
+    torch::Tensor FAFN = this->FAF();
+    torch::Tensor FAFP = this->FAF();
+    return std::make_tuple(FAFN, FAFP);
+}
+
+
+torch::Tensor MeshTensor::dilatedFaceAdjacencies(int const& dilation){
+    if (dilation <= 1){
+        throw std::invalid_argument("dilation must be greater than 1");
+    } else {
+        // TODO implement the CUDA kernel
+        throw std::invalid_argument("not implemented");
+    }
+    return torch::zeros({this->faces.size(0), this->faces.size(1), 3});
+}
+
+
+torch::Tensor MeshTensor::convolutionKernelPattern(int const& kernel_size, int const& dilation){
+    if (kernel_size==3){
+        if (dilation==1){
+            return this->FAF();
+        } else {
+            return this->dilatedFaceAdjacencies(dilation);
+        }
+    } else if (kernel_size==5){
+        throw std::invalid_argument("not implemented");
+        if (dilation==1){
+            return torch::cat(
+                // FAF* is 3d tensor
+                // 0: no meshes
+                // 1: no of faces
+                // 2: no of adjacent faces (3)
+                {
+                    this->FAFN().slice(2, 0, 1),
+                    this->FAF().slice(2, 0, 1),
+                    this->FAFP().slice(2, 0, 1),
+                    this->FAFN().slice(2, 1, 2),
+                    this->FAF().slice(2, 1, 2),
+                    this->FAFP().slice(2, 1, 2),
+                    this->FAFN().slice(2, 2, 3),
+                    this->FAF().slice(2, 2, 3),
+                    this->FAFP().slice(2, 2, 3)
+                }, 2);
+        } else {
+            throw std::invalid_argument("not implemented");
+        }
+    } else {
+        // TODO implement the CUDA kernel
+    }
+}
 
 // operations
-MeshTensor MeshTensor::operator+(MeshTensor const& other) const{
+MeshTensor MeshTensor::operator+(MeshTensor & other) const{
     // adds the features of two meshes together
-    torch::Tensor feats = this->feats + other.feats;
-    return MeshTensor(feats, this->faces, this->cache);
+    torch::Tensor addedFeats = this->feats + other.feats;
+    return MeshTensor(faces, addedFeats, Fs, cache);
 }
 
-MeshTensor MeshTensor::operator-(MeshTensor const& other) const{
+MeshTensor MeshTensor::operator-(MeshTensor & other) const{
     // subtracts the features of two meshes together
-    torch::Tensor feats = this->feats - other.feats;
-    return MeshTensor(feats, this->faces, this->cache);
+    torch::Tensor subtractedFeats = this->feats - other.feats;
+    return MeshTensor(faces, subtractedFeats, Fs, cache);
 }
 
 
 // properties
-std::vector<int> MeshTensor::shape(){
-    auto shape = this->feats.sizes();
-    return std::vector<int>(shape.begin(), shape.end());
-}
-
-
-int MeshTensor::V(){
-    /* Maximum number of vertices in the mini-batch */
-
-    auto it = this->cache.find("V");
-    if (it != this->cache.end()){
-        return this->cache["V"][0];
-    } else{
-        float V = this->faces.max().item<int>();
-        this->cache["V"] = {V};
-        return int(V);
-    }
-    
-}
-
-
-// std::vector<float> MeshTensor::Vs(){
-//     /*
-//     Number of vertices in each mesh. 
-    
-//     Returns
-//     ------------
-//     (N,) int32
-//     */
-//     auto it = this->cache.find("Vs");
-//     if (it != this->cache.end()){
-//         return this->cache["Vs"];
-//     } else {
-//         auto Vs = torch::amax(this->faces, 1).item<float>();
-//         return std::vector<float>(Vs.begin<int>(), Vs.end<int>());
-//     }
-
-// }
 
 
 // std::vector<int> MeshTensor::degrees(){
@@ -147,6 +350,29 @@ int MeshTensor::V(){
 // }
 
 
-std::vector<int> MeshTensor::FAF(){}
-std::vector<int> MeshTensor::FAFP(){}
-std::vector<int> MeshTensor::FAFN(){}
+torch::Tensor MeshTensor::FAF(){
+    if (this->cache.find("FAF") == this->cache.end()){
+        this->cache["FAF"] = this->computeFaceAdjacencyFaces();
+    } else {
+        return this->cache["FAF"];
+    }
+}
+
+
+torch::Tensor MeshTensor::FAFP(){
+    if (this->cache.find("FAFP") == this->cache.end()){
+        this->cache["FAFP"] = std::get<1>(this->computeFaceAdjacencyReordered());
+    } else {
+        return this->cache["FAFP"];
+    }
+}
+
+
+torch::Tensor MeshTensor::FAFN(){
+    if (this->cache.find("FAFN") == this->cache.end()){
+        this->cache["FAFN"] = std::get<0>(this->computeFaceAdjacencyReordered());
+    } else {
+        return this->cache["FAFN"];
+    }
+}
+
--- source/subdivnet/mesh_tensor.h
+++ source/subdivnet/mesh_tensor.h
@@ -3,7 +3,23 @@
 #include <map>
 #include <optional>
 
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/take.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/view/zip.hpp>
+
+#include <givde/lib/dggs/multiatlas.h>
+
 #include <torch/torch.h>
+
+// #include "utils.h"
+
+#include "parallel.cu"
+
+
+using Index = givde::multiatlas::Index;
+using IndexList = std::vector<givde::multiatlas::Index>;
 
 
 class MeshTensor {
@@ -16,40 +32,110 @@ class MeshTensor {
     number of faces. 
     */
     public:
+        /*
+        Parameters
+        ------------
+        faces: (N, F, 3) int32
+            Array of triangular faces.
+        feats: (N, C, F) float32
+            Array of face features.
+        Fs: (N, 1) int32, optional
+            Array of number of faces in each mesh. 
+            If not specified, Fs is set to n.
+        cache: dict, optional
+            things calculated from faces to avoid repeated calculation for the 
+            same mesh.
+        */
+        // DGGSTensor dggsTensor;
+        givde::multiatlas::DGGS madggs = givde::multiatlas::DGGS(
+            givde::geometry::icosahedron(),
+            givde::multiatlas::LatLngLookupGridArgs{10, 10},
+            givde::multiatlas::NormalProjection{}
+        );
+        std::unordered_map<int, givde::multiatlas::Index> faceIdxMap;
+        std::unordered_map<givde::multiatlas::Index, int> idxFaceMap;
+        givde::Resolution resolution = givde::Resolution(5);
+        givde::geometry::IGLGeometry dggsMesh;
+
         torch::Tensor faces;
         torch::Tensor feats;
-        int Fs;
-        std::map<std::string, std::vector<float>> cache;
+        std::vector<int> Fs;
+        std::unordered_map<std::string, torch::Tensor> cache;
+        int N, F, C;
 
-        MeshTensor(torch::Tensor faces, 
-                    torch::Tensor feats,
-                    std::map<std::string, std::vector<float>> cache);
+        MeshTensor(
+            torch::Tensor faces, 
+            torch::Tensor feats,
+            std::vector<int> Fs,
+            std::unordered_map<std::string, torch::Tensor> cache);
 
 
-        MeshTensor updated(torch::Tensor tensor);
+        MeshTensor updated(torch::Tensor & tensor);
         MeshTensor inverseLoopPool(
             std::string op="max", 
             std::optional<torch::Tensor> pooled_feats=std::nullopt
         );
-        MeshTensor loopSubdivision();
-        MeshTensor loopUnPool();
-        std::vector<int> computeFaceAdjacencyFaces();
-        std::vector<int> computeFaceAdjacencyReordered();
-        MeshTensor dilatedFaceAdjacencies(); // should return jit code result
-        MeshTensor convolutionKernelPattern(); // should return jit code result
+        // torch::Tensor loopSubdivision();
+        // MeshTensor loopUnPool(
+        //     std::string mode, 
+        //     torch::Tensor ref_faces = torch::Tensor(), 
+        //     std::map<std::string, std::vector<float>> ref_cache 
+        //                     = std::map<std::string, std::vector<float>>());
+        torch::Tensor computeFaceAdjacencyFaces();
+        std::tuple<torch::Tensor, torch::Tensor> computeFaceAdjacencyReordered();
+        torch::Tensor dilatedFaceAdjacencies(int const& dilation); // should return jit code result
+        torch::Tensor convolutionKernelPattern(int const& kernel_size=3, int const& dilation=1); // should return jit code result
         std::vector<float> aggregateVertexFeatures();
 
         // operations
-        MeshTensor operator+(MeshTensor const& other) const;
-        MeshTensor operator-(MeshTensor const& other) const;
+        MeshTensor operator+(MeshTensor & other) const;
+        MeshTensor operator-(MeshTensor & other) const;
         
 
         // properties
-        std::vector<int> shape();
-        int V();
-        std::vector<float> Vs();
-        std::vector<int> degrees();
-        std::vector<int> FAF();
-        std::vector<int> FAFP();
-        std::vector<int> FAFN();
+        std::vector<int> shape(){
+            auto shape = this->faces.sizes();
+            return std::vector<int>(shape.begin(), shape.end());
+        };
+
+
+        // int V(){
+        //     if (this->cache.find("V") == this->cache.end()){
+        //         this->cache["V"] = {(float)this->faces.max().item<int>()};
+        //     }
+        //     return this->cache["V"].item<int>();
+        // };
+
+        // std::vector<int> Vs(){
+        //     if (this->cache.find("Vs") == this->cache.end()){
+        //         auto Vs = torch::amax(this->faces, 1).item<float>();
+        //         return std::vector<float>(Vs.begin<int>(), Vs.end<int>());
+        //         this->cache["Vs"] = {(float)this->faces.max(1).values.max(0).values.item<int>()};
+        //     }
+        //     return this->cache["Vs"];
+        // };
+
+
+        torch::Tensor FAF();
+        torch::Tensor FAFP();
+        torch::Tensor FAFN();
 };
+
+
+
+IndexList makeIndex(
+    givde::multiatlas::DGGS const &madggs,
+    givde::Resolution const& resolution);
+
+
+auto idxsToMesh(
+    givde::multiatlas::DGGS const &dggs,
+    IndexList const &idxs
+    // std::vector<givde::Vec3<givde::f64>> const &colors
+) -> std::tuple<
+    givde::geometry::IGLGeometry, 
+    std::tuple<
+        std::unordered_map<int, givde::multiatlas::Index>, 
+        std::unordered_map<givde::multiatlas::Index, int>
+        >
+    >;
